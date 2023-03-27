95b8f6d6f624d755730f2b1995259c639c53cfed
Adnan Ahmed <adnan.ahmed01@gmail.com>
2023-03-27 23:10:05 +0100

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import { LaunchListPage } from './pages/LaunchListPage'
+import { HomePage } from './pages/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <LaunchListPage />
+    <GlobalStyle />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/launches" element={<LaunchListPage />}  />
+        </Routes>
     </div>
   );
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+
+export const HomePage = () => {
+  return (
+    <>
+     <p>Testing</p>
+    </>
+  );
+};
